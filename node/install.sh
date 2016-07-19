6393576c80c0a4813b97824a58132882746e1ef6
amingilani <aminshahgilani@gmail.com>
2016-07-19 22:29:27 +0500

    Add new packages to be installed via node and cask

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,9 @@
 if test ! $(which spoof)
 then
-  npm install spoof -g
+  echo 'installing npm packages'
+  npm i -g spoof
+  npm i -g jshint
+  npm i -g yo
+  npm i -g grunt-cli
+  npm i -g generator-gitignore
 fi
