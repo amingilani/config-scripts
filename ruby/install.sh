cd543315d96657ea4b7db1c3196018f80073f00c
amingilani <aminshahgilani@gmail.com>
2016-07-22 06:41:21 +0500

    Add conditional nvm and rvm installations and move nvm config to nvm topic

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,11 +1,20 @@
 echo 'installing rvm, ruby and rails'
-# get mpapis' public key
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-# install rvm with ruby and rails
-\curl -sSL https://get.rvm.io | bash -s stable --rails
 
-echo ''
-echo 'installing gems'
+#!/bin/bash
+
+# if nvm is not installed
+if ! type rvm
+then
+  # install nvm
+  echo 'Installing nvm'
+
+  # get mpapis' public key
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+  # install rvm with ruby and rails
+  \curl -sSL https://get.rvm.io | bash -s stable --rails
+fi
+
+echo 'Installing gems'
 gem install bundler
 gem install rubocop
 gem install rspec
