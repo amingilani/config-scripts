52e10f1cad6f4716af255167540c89e380a6d0c9
amingilani <aminshahgilani@gmail.com>
2016-09-28 18:48:15 +0500

    My ssh key from now on will be an ed25517 key

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,3 +1,3 @@
 # Pipe my public key to my clipboard.
-alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
-export SSH_KEY_PATH="~/.ssh/id_rsa"
+alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+export SSH_KEY_PATH="~/.ssh/id_ed25519"
